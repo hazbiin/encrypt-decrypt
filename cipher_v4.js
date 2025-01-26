8b166390dcda275e9fcf3df43d87271d9bdf3fa1
hasbin <hzbi717@gmail.com>
2025-01-26 17:43:07 +0530

    encryption done

--- cipher_v4.js
+++ cipher_v4.js
@@ -1,57 +1,40 @@
-const messageInput = document.getElementById('plain-text').value;
-const encryptionKey = parseInt(document.getElementById('encryption-key').value);
-const encryptBtn = document.getElementById('encrypt-btn');
 const cipherOutput = document.getElementById('cipher-text');
+const encryptBtn = document.getElementById('encrypt-btn');
 const decryptBtn  = document.getElementById('decrypt-btn');
-let alphabets = 'abcdefghijklmnopqrstuvwxyz';
-let cipherText = '';
 
-// cipherOutput.value = '';
-// cipherOutput.value = encrypt(messageInput,encryptionKey);
+let alphabets = 'abcdefghijklmnopqrstuvwxyz';
+
 function encrypt(messageInput, encryptionKey){
+    let cipherText = '';
+    encryptionKey = parseInt(encryptionKey);
 
     for(let character of messageInput){
 
+        // handling special character and numbers
         let charCode = character.charCodeAt(0);
-        // special character checking 
-        if(!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || (charCode >= 48 && charCode <= 57))){
-            cipherText += character;
-        }
-
         if(!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122))){
             cipherText += character;
+            continue;
         }
 
-        
-        if(character === character.toUpperCase() && character !== character.toLowerCase()){
+        let isUpperCase = character === character.toUpperCase();
+        if(isUpperCase){
             character = character.toLowerCase();
-
-
-    
-            for(let i = 0; i < alphabets.length; i++){
-                if(character === alphabets[i]){
-                    let currentIndex = i;
-                    let newIndex = (currentIndex + encryptionKey) % alphabets.length;
-                    cipherText += alphabets[newIndex].toUpperCase();
-                }
-            }
-
-        }else {
-            for(let i = 0; i < alphabets.length; i++){
-                if(character === alphabets[i]){
-                    let currentIndex = i;
-                    let newIndex = (currentIndex + encryptionKey) % alphabets.length;
-                    cipherText += alphabets[newIndex];
-                }
-            }
         }
 
+        for(let i = 0; i < alphabets.length; i++){
+            if(character === alphabets[i]){
+                let currentIndex = i;
+                let newIndex = (currentIndex + encryptionKey) % alphabets.length;
+                let shiftedChar = alphabets[newIndex];
 
+                cipherText += isUpperCase ? shiftedChar.toUpperCase() : shiftedChar;
+                break;
+            }
+        }
     }
-
     return cipherText;
 }
-
 
 
 encryptBtn.addEventListener("click", () => {
